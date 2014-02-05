3315bf18984554e8df3cf9986385edcb09a341c0
Grzegorz Hadala <grzegorz.hadala@adpilot.pl>
2014-02-05 17:31:07 +0100

    work

---
+++ homepage.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Template name: Clear Homepage
+*/
+
+get_header();
+?>
+
+
+<a href="http://www.hadala.pl" class="logo" style="margin-top: 200px;"><img src="<?php echo get_template_directory_uri(); ?>/img/logo_gh4.png" alt=""></a>
+
+
+<?php
+get_footer();
+
